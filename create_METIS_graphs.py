f6a1b2196c7389e5bd157a772cb8db5e4ac7b8e6
sajjad.ahmad <sajjad.ahmad@cas.de>
2022-06-09 11:43:39 +0200

    Refactored and Added code to allow for parallel creation and writing of METIS graphs.

---
+++ create_METIS_graphs.py
@@ -0,0 +1,89 @@
+# creates and returns a VIG from a file
+# assumes that the file is in DIMACS CNF format
+import math
+import os
+import sys
+from typing import Final
+import networkit as nk
+import time
+import pandas as pd
+
+SRC_DIR: Final[str] = sys.argv[1]
+COLUMNS: Final = ['ID', 'Time to create graph', 'Time to write graph', 'Space Needed']
+GRAPH_OUTPUT_DIR: Final[str] = "data/created_vigs/"
+MEASURED_OUTPUT_DIR: Final[str] = "data/measured_data/"
+FILE_ENDING: Final[str] = "_time_and_size_data.csv"
+
+
+def create_vig_from_file(path: str):
+    with open(path) as file:
+        for line in file:
+            words = line.split()
+            # ignore comments and unit clauses
+            if len(words) <= 2 or words[0] == str('c'):
+                continue
+            # since each line has a '0' as EOL we have to rem 1 from len
+            bin_coeff = math.comb(len(words) - 1, 2)
+            weight = 1.0 / bin_coeff
+            if words[0] == str('p') and words[1] == 'cnf' and len(words) >= 4:
+                # the first line gives is num_vars and num_clauses
+                num_vars = int(words[2])
+                num_clauses = int(words[3])
+                file_graph = nk.Graph(num_vars, True)
+            else:
+                # we have to remove the '0' from the len as EOL marker
+                curr_len = len(words) - 1
+                for i in range(curr_len):
+                    for j in range(i + 1, curr_len):
+                        # since indices in the graph are 0-based, but in CNF they are 1-based
+                        node_a = abs(int(words[i])) - 1
+                        node_b = abs(int(words[j])) - 1
+                        if not file_graph.hasEdge(node_a, node_b):
+                            file_graph.addEdge(node_a, node_b, weight)
+                        else:
+                            file_graph.increaseWeight(node_a, node_b, weight)
+    return file_graph
+
+
+# write graph to a file and return the file size
+def write_graph_to_METIS(test_graph, output_path):
+    if not os.path.isdir(GRAPH_OUTPUT_DIR):
+        os.makedirs(GRAPH_OUTPUT_DIR)
+    # example_path: str = OUTPUT_PATH + "000a41cdca43be89ed62ea3abf2d0b64-snw_13_9_pre"
+
+    graph_file = output_path + "_METIS"
+    nk.writeGraph(test_graph, graph_file, nk.Format.METIS)
+    return os.path.getsize(graph_file)
+
+
+# script receives file path  of a .cnf as input parameter
+if __name__ == '__main__':
+    # to strip ending of file path, e.g. '.cnf'
+    file_name = os.path.basename(SRC_DIR)
+    file_name_without_ending = os.path.splitext(file_name)[0]
+    full_output_path = GRAPH_OUTPUT_DIR + file_name_without_ending
+    # we will use the file names for the id during the rest of the project
+    current_id = file_name_without_ending
+    # we will measure time from reading the cnf to creating the graph
+    # and from having the graph to writing it in a file
+    time_start = time.process_time()
+    graph = create_vig_from_file(SRC_DIR)
+    time_create_graph = time.process_time() - time_start
+    print(graph.numberOfNodes(), graph.numberOfEdges())
+    print("Time taken to create graph: %f", time_create_graph)
+
+    time_start = time.process_time()
+    file_size = write_graph_to_METIS(graph, full_output_path)
+    time_write_graph = time.process_time() - time_start
+
+    data = {
+        COLUMNS[0]: [current_id],
+        COLUMNS[1]: [time_create_graph],
+        COLUMNS[2]: [time_write_graph],
+        COLUMNS[3]: [file_size]
+    }
+    df = pd.DataFrame(data, columns=COLUMNS)
+    df.set_index(COLUMNS[0], inplace=True)
+    # save to csv
+    os.makedirs(os.path.dirname(MEASURED_OUTPUT_DIR), exist_ok=True)
+    df.to_csv(MEASURED_OUTPUT_DIR + file_name_without_ending + FILE_ENDING)
---
+++ evaluate_graph_formats.py
@@ -0,0 +1,43 @@
+import os
+from typing import Final
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+COLUMNS: Final = {'Measure', 'EdgeListSpaceOne', 'METIS', 'ThrillBinary', 'NetworkitBinary'}
+
+
+def evaluate_graph_formats(size_table_path, time_table_path):
+    if os.path.isfile(size_table_path) and os.path.isfile(time_table_path):
+        read_time_df = pd.read_csv(time_table_path, index_col=0)
+        read_size_df = pd.read_csv(size_table_path, index_col=0)
+
+        time_table = pd.DataFrame(columns=COLUMNS)
+        size_table = pd.DataFrame(columns=COLUMNS)
+
+        # TODO: Create proper table and decide on format
+        time_means = read_time_df.mean()
+        time_median = read_time_df.median()
+        time_025_quantile = read_time_df.quantile(0.25)
+        time_075_quantile = read_time_df.quantile(0.75)
+
+        size_means = read_size_df.mean()
+        size_median = read_size_df.median()
+        size_025_quantile = read_size_df.quantile(0.25)
+        size_075_quantile = read_size_df.quantile(0.75)
+
+        time_boxplot = read_time_df.boxplot(showmeans=True)
+        time_boxplot.set_ylabel('Time in s')
+        plt.title('Graph-Format Comparison of Writing-Times')
+        plt.show()
+        size_boxplot = read_size_df.boxplot(showmeans=True)
+        size_boxplot.set_ylabel('File size in Bytes')
+        plt.title('Graph-Format Comparison of File Sizes')
+        plt.show()
+
+
+# script takes in the measured time and size for diff. graph formats and creates boxplots for evaluation
+if __name__ == '__main__':
+    size_csv_path = "data/measured_data/size_taken_to_write_graph.csv"
+    time_csv_path = "data/measured_data/time_taken_to_write_graph.csv"
+    evaluate_graph_formats(size_csv_path, time_csv_path)
