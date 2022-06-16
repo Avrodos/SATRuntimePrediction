6709e405d1b92aa2db3573839d9f4b3fc13836ee
sajjad.ahmad <sajjad.ahmad@cas.de>
2022-06-16 11:35:36 +0200

    Added num_nodes and num_edges feature. Distributions are now represented using 5 stats: mean, min, max, variance and entropy.

--- extract_features.py
+++ extract_features.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+from math import e, log
 from typing import Final
 import networkit as nk
 import numpy as np
@@ -12,6 +13,33 @@ FEATURE_OUTPUT_DIR: Final[str] = 'data/measured_data/'
 FEATURE_FILE_ENDING: Final[str] = '_features'
 TIME_FILE_ENDING: Final[str] = '_time'
 
+
+# helpers:
+# Computes entropy of array distribution.
+# fastest entropy calculation taken from:
+# https://stackoverflow.com/questions/15450192/fastest-way-to-compute-entropy-in-python
+def calculate_entropy(given_array, base=None):
+
+    len_array = len(given_array)
+
+    if len_array <= 1:
+        return 0
+
+    value, counts = np.unique(given_array, return_counts=True)
+    probs = counts / len_array
+    n_classes = np.count_nonzero(probs)
+
+    if n_classes <= 1:
+        return 0
+
+    ent = 0.
+
+    # Compute entropy
+    base = e if base is None else base
+    for i in probs:
+        ent -= i * log(i, base)
+
+    return ent
 
 # A connected component is a set of nodes in which each pair of nodes is connected by a path.
 # returns two df: data_df & time_data_df
@@ -25,14 +53,27 @@ def calculate_components(given_graph):
     time_df = pd.DataFrame({'Time_#ConnectedComponents': [time_connected_components]})
     return data_df, time_df
 
+# TODO: degree assortativity
+
+# TODO: graph core
+
+# TODO: Also calculate edge cut fraction
 
 # calculates centrality measures
+# a distribution is measured using 5 stats:
+# mean, min, max, variance & entropy
 # returns two df: data_df & time_data_df
 def calculate_centrality(given_graph):
     time_start = time.process_time()
     deg_cen = nk.centrality.DegreeCentrality(given_graph)
     deg_cen.run()
-    mean_deg_cen = np.mean(np.array(deg_cen.scores(), float))
+
+    np_array_deg_cen = np.array(deg_cen.scores(), float)
+    mean_deg_cen = np.mean(np_array_deg_cen)
+    min_deg_cen = np.min(np_array_deg_cen)
+    max_deg_cen = np.max(np_array_deg_cen)
+    var_deg_cen = np.var(np_array_deg_cen)
+    entropy_deg_cen = calculate_entropy(np_array_deg_cen)
     time_deg_cen = time.process_time() - time_start
 
     time_start = time.process_time()
@@ -42,44 +83,89 @@ def calculate_centrality(given_graph):
     time_start = time.process_time()
     eigenvector_cen = nk.centrality.EigenvectorCentrality(given_graph)
     eigenvector_cen.run()
-    mean_eigenvector_cen = np.mean(np.array(eigenvector_cen.scores(), float))
+    np_array_eigenvector_cen = np.array(eigenvector_cen.scores(), float)
+    mean_eigenvector_cen = np.mean(np_array_eigenvector_cen)
+    min_eigenvector_cen = np.min(np_array_eigenvector_cen)
+    max_eigenvector_cen = np.max(np_array_eigenvector_cen)
+    var_eigenvector_cen = np.var(np_array_eigenvector_cen)
+    entropy_eigenvector_cen = calculate_entropy(np_array_eigenvector_cen)
     time_eigenvector_cen = time.process_time() - time_start
 
     time_start = time.process_time()
     page_rank = nk.centrality.PageRank(given_graph)
     page_rank.run()
-    mean_page_rank = np.mean(np.array(page_rank.scores(), float))
+    np_array_page_rank = np.array(page_rank.scores(), float)
+    mean_page_rank = np.mean(np_array_page_rank)
+    min_page_rank = np.min(np_array_page_rank)
+    max_page_rank = np.max(np_array_page_rank)
+    var_page_rank = np.var(np_array_page_rank)
+    entropy_page_rank = calculate_entropy(np_array_page_rank)
     time_page_rank = time.process_time() - time_start
 
     time_start = time.process_time()
     katz_cen = nk.centrality.KatzCentrality(given_graph)
     katz_cen.run()
-    mean_katz_cen = np.mean(np.array(katz_cen.scores(), float))
+    np_array_katz_cen = np.array(katz_cen.scores(), float)
+    mean_katz_cen = np.mean(np_array_katz_cen)
+    min_katz_cen = np.min(np_array_katz_cen)
+    max_katz_cen = np.max(np_array_katz_cen)
+    var_katz_cen = np.var(np_array_katz_cen)
+    entropy_katz_cen = calculate_entropy(np_array_katz_cen)
     time_katz_cen = time.process_time() - time_start
 
     time_start = time.process_time()
     betweenness_cen = nk.centrality.EstimateBetweenness(given_graph, 50)  # (graph, nSamples)
     betweenness_cen.run()
-    mean_betweenness_cen = np.mean(np.array(betweenness_cen.scores(), float))
+    np_array_betweenness_cen = np.array(betweenness_cen.scores(), float)
+    mean_betweenness_cen = np.mean(np_array_betweenness_cen)
+    min_betweenness_cen = np.min(np_array_betweenness_cen)
+    max_betweenness_cen = np.max(np_array_betweenness_cen)
+    var_betweenness_cen = np.var(np_array_betweenness_cen)
+    entropy_betweenness_cen = calculate_entropy(np_array_betweenness_cen)
     time_betweenness_cen = time.process_time() - time_start
 
     # putting it all into a df
     data = {
         'MeanDegreeCentrality': [mean_deg_cen],
+        'MinDegreeCentrality': [min_deg_cen],
+        'MaxDegreeCentrality': [max_deg_cen],
+        'VarDegreeCentrality': [var_deg_cen],
+        'EntropyDegreeCentrality': [entropy_deg_cen],
+
         'Centralization': [centralization],
+
         'MeanEigenvectorCentrality': [mean_eigenvector_cen],
+        'MinEigenvectorCentrality': [min_eigenvector_cen],
+        'MaxEigenvectorCentrality': [max_eigenvector_cen],
+        'VarEigenvectorCentrality': [var_eigenvector_cen],
+        'EntropyEigenvectorCentrality': [entropy_eigenvector_cen],
+
         'MeanPageRank': [mean_page_rank],
+        'MinPageRank': [min_page_rank],
+        'MaxPageRank': [max_page_rank],
+        'VarPageRank': [var_page_rank],
+        'EntropyPageRank': [entropy_page_rank],
+
         'MeanKatzCentrality': [mean_katz_cen],
-        'MeanBetweennessCentrality': [mean_betweenness_cen]
+        'MinKatzCentrality': [min_katz_cen],
+        'MaxKatzCentrality': [max_katz_cen],
+        'VarKatzCentrality': [var_katz_cen],
+        'EntropyKatzCentrality': [entropy_katz_cen],
+
+        'MeanBetweennessCentrality': [mean_betweenness_cen],
+        'MinBetweennessCentrality': [min_betweenness_cen],
+        'MaxBetweennessCentrality': [max_betweenness_cen],
+        'VarBetweennessCentrality': [var_betweenness_cen],
+        'EntropyBetweennessCentrality': [entropy_betweenness_cen]
     }
 
     time_data = {
-        'Time_MeanDegreeCentrality': [time_deg_cen],
+        'Time_DegreeCentrality': [time_deg_cen],
         'Time_Centralization': [time_centralization],
-        'Time_MeanEigenvectorCentrality': [time_eigenvector_cen],
-        'Time_MeanPageRank': [time_page_rank],
-        'Time_MeanKatzCentrality': [mean_katz_cen],
-        'Time_MeanBetweennessCentrality': [time_betweenness_cen]
+        'Time_EigenvectorCentrality': [time_eigenvector_cen],
+        'Time_PageRank': [time_page_rank],
+        'Time_KatzCentrality': [mean_katz_cen],
+        'Time_BetweennessCentrality': [time_betweenness_cen]
     }
     return pd.DataFrame(data), pd.DataFrame(time_data)
 
@@ -93,13 +179,17 @@ def calculate_community(given_graph):
 
     time_start = time.process_time()
     num_communities = communities.numberOfElements()
-    mean_community_size = np.mean(np.array(communities.subsetSizes(), float))
-    time_mean_community_size = time_communities + time.process_time() - time_start
+    np_array_community_size = np.array(communities.subsetSizes(), float)
+    mean_community_size = np.mean(np_array_community_size)
+    min_community_size = np.min(np_array_community_size)
+    max_community_size = np.max(np_array_community_size)
+    var_community_size = np.var(np_array_community_size)
+    entropy_community_size = calculate_entropy(np_array_community_size)
+    time_community_size = time_communities + time.process_time() - time_start
 
-    min_community_size = min(communities.subsetSizes())
-    max_community_size = max(communities.subsetSizes())
-    # for simplicity’s sake, I will round up, such that min and max calculation
-    # take up the same time as mean
+    time_start = time.process_time()
+    imbalance = nk.community.GraphClusteringTools().getImbalance(communities)
+    time_imbalance = time_communities + time.process_time() - time_start
 
     time_start = time.process_time()
     intercommunity_edge_weight = nk.community.EdgeCut().getQuality(communities, given_graph)
@@ -109,23 +199,39 @@ def calculate_community(given_graph):
     modularity = nk.community.Modularity().getQuality(communities, given_graph)
     time_modularity = time_communities + time.process_time() - time_start
 
+    time_start = time.process_time()
+    hub_dominance = nk.community.HubDominance().getQuality(communities, given_graph)
+    time_hub_dominance = time_communities + time.process_time() - time_start
+
+    time_start = time.process_time()
+    intrapartition_density_algo = nk.community.IntrapartitionDensity(given_graph, communities)
+    intrapartition_density_algo.run()
+    global_intrapartition_density = intrapartition_density_algo.getGlobal()
+    time_intrapartition_density = time_communities + time.process_time() - time_start
+
     # putting it all into two df
     data = {
         '#Communities': [num_communities],
+        'MeanCommunitySize': [mean_community_size],
         'MinCommunitySize': [min_community_size],
         'MaxCommunitySize': [max_community_size],
-        'MeanCommunitySize': [mean_community_size],
+        'VarCommunitySize': [var_community_size],
+        'EntropyCommunitySize': [entropy_community_size],
+        'ClusterImbalance': [imbalance],
         'TotalInter-CommunityEdgeWeight': [intercommunity_edge_weight],
-        'Modularity': [modularity]
+        'Modularity': [modularity],
+        'HubDominance': [hub_dominance],
+        'GlobalIntrapartitionDensity': [global_intrapartition_density]
     }
 
     time_data = {
         'Time_#Communities': [time_communities],
-        'Time_MinCommunitySize': [time_mean_community_size],
-        'Time_MaxCommunitySize': [time_mean_community_size],
-        'Time_MeanCommunitySize': [time_mean_community_size],
+        'Time_CommunitySize': [time_community_size],
+        'Time_ClusterImbalance': [time_imbalance],
         'Time_TotalInter-CommunityEdgeWeight': [time_intercommunity_edge_weight],
-        'Time_Modularity': [time_modularity]
+        'Time_Modularity': [time_modularity],
+        'Time_HubDominance': [time_hub_dominance],
+        'Time_GlobalIntrapartitionDensity': [time_intrapartition_density]
     }
     return pd.DataFrame(data), pd.DataFrame(time_data)
 
@@ -137,6 +243,7 @@ def calculate_clustering(given_graph):
     global_clustering_coefficient = nk.globals.clustering(given_graph)
     time_clustering = time.process_time() - time_start
 
+    # TODO: more clustering coefficients
     # putting it into two df
     data_df = pd.DataFrame({'GlobalClusteringCoefficient': [global_clustering_coefficient]})
     time_df = pd.DataFrame({'Time_GlobalClusteringCoefficient': [time_clustering]})
@@ -171,9 +278,10 @@ def calculate_diameters(given_graph):
     return data_df, time_df
 
 
+# Takes too long for current purpose
 def calculate_small_world(given_graph):
     time_start = time.process_time()
-    nx_graph = nk.nxadapter.nk2nx(given_graph) # convert from NetworKit.Graph to networkx.Graph
+    nx_graph = nk.nxadapter.nk2nx(given_graph)  # convert from NetworKit.Graph to networkx.Graph
     time_graph_conversion = time.process_time() - time_start
 
     time_start = time.process_time()
@@ -187,8 +295,8 @@ def calculate_small_world(given_graph):
     time_omega = time_graph_conversion + time.process_time() - time_start
 
     # putting it into two df
-    data_df = pd.DataFrame({'SmallWorldSigma': [sigma], 'SmallWorldOmega': [omega]})
-    time_df = pd.DataFrame({'Time_SmallWorldSigma': [time_sigma], 'Time_SmallWorldOmega': [time_omega]})
+    data_df = pd.DataFrame({'SmallWorldSigma': [], 'SmallWorldOmega': [omega]})
+    time_df = pd.DataFrame({'Time_SmallWorldSigma': [], 'Time_SmallWorldOmega': [time_omega]})
     return data_df, time_df
 
 
@@ -203,12 +311,16 @@ if __name__ == '__main__':
     full_output_path_time = FEATURE_OUTPUT_DIR + current_id + TIME_FILE_ENDING
 
     # read the graph from file
+    main_time_start = time.process_time()
     metisReader = nk.graphio.METISGraphReader()
     graph = metisReader.read(SRC_DIR)
+    num_nodes = graph.numberOfNodes()
+    num_edges = graph.numberOfEdges()
+    time_read_graph = time.process_time() - main_time_start
     # our feature df which should contain all features in the end
-    feature_df = pd.DataFrame({'ID': [current_id]})
+    feature_df = pd.DataFrame({'ID': [current_id], '#Nodes': [num_nodes], '#Edges': [num_edges]})
     # time df which tells us how long the computation of each feature took
-    time_feature_df = pd.DataFrame({'ID': [current_id]})
+    time_feature_df = pd.DataFrame({'ID': [current_id], 'Time_ReadGraph': [time_read_graph]})
 
     # calculate and concatenate features
     # connected components
@@ -227,15 +339,16 @@ if __name__ == '__main__':
     diameter_features, time_diameter_features = calculate_diameters(graph)
 
     # small-world measures
-    small_world_features, time_small_world_features = calculate_small_world(graph)
+    # small_world_features, time_small_world_features = calculate_small_world(graph)
 
     # put it all into corresponding dataframes
     feature_df = pd.concat([feature_df, component_features,
-                            centrality_features, community_features], axis=1)
+                            centrality_features, community_features, clustering_features, diameter_features], axis=1)
     feature_df.set_index('ID', inplace=True)
 
-    time_feature_df = pd.concat([time_feature_df, time_component_features,
-                                 time_centrality_features, time_community_features], axis=1)
+    time_feature_df = pd.concat(
+        [time_feature_df, time_component_features, time_centrality_features, time_community_features,
+         time_clustering_features, time_diameter_features], axis=1)
     time_feature_df.set_index('ID', inplace=True)
 
     feature_df.to_csv(full_output_path_features)
